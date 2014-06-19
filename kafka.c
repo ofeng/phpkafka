cadb154c6f1127fdda9d268fffbf359ff1f384f5
Patrick Reilly <preilly@php.net>
2014-06-19 11:39:02 -0700

    fix value of exit_eof to extra

--- kafka.c
+++ kafka.c
@@ -60,7 +60,7 @@ static struct conf {
         .partition = RD_KAFKA_PARTITION_UA,
         .msg_size = 1024*1024,
         .delim = '\n',
-        .exit_eof = 0,
+        .exit_eof = 1,
 };
 
 void kafka_connect(char *brokers)
